d02fdcf9e2a4e9780816f729f3a70dfa7122a30b
Carlos7Orteg <131712489+Carlos7Orteg@users.noreply.github.com>
2023-10-24 07:00:47 +0000

    Deploying to gh-pages from @ Carlos7Orteg/proyecto-fs-team2@990287c09e8e277043994b9c68cab90b6d5e12a7 🚀

---
+++ index.1683dfcd.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".link").forEach((function(e){e.addEventListener("click",(function(t){t.preventDefault();var n=e.getAttribute("href");window.location.href=n}))}));
+//# sourceMappingURL=index.1683dfcd.js.map
---
+++ index.fc210251.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".link").forEach((e=>{e.addEventListener("click",(t=>{t.preventDefault();const n=e.getAttribute("href");window.location.href=n}))}));
+//# sourceMappingURL=index.fc210251.js.map
